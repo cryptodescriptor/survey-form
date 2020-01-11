93cb4661e5ca52910a4025844bee02ef359f1640
cryptodescriptor <cryptodescriptor@hotmail.com>
2020-01-11 00:29:00 +0000

    Improved accessibility by outlining radio, checkbox, and select elements when focused using TAB.

---
+++ highlightTabFocus.js
@@ -0,0 +1,63 @@
+/* Highlight checkboxes and radio buttons only when focused using TAB */
+
+var tabbed = false;
+
+var keyCodes = [
+	9 /* TAB */,
+	37 /* LEFT */,
+	38 /* UP */,
+	39 /* RIGHT */,
+	40 /* DOWN */
+];
+
+window.addEventListener('keydown', function(e) {
+  if (keyCodes.indexOf(e.keyCode) !== -1) {
+    tabbed = true;
+  }
+}, true); /* as soon as captured */
+
+window.addEventListener('mousedown', function(e) {
+  tabbed = false;
+}, true); /* as soon as captured */
+
+var radios = document.querySelectorAll('input[type="radio"]'),
+	checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+// ie11 nodelist forEach
+if(window.NodeList && !NodeList.prototype.forEach) {
+	NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+var cachedAdjacentElements = {};
+
+[radios, checkboxes].forEach(function(elements) {
+	elements.forEach(function(element) {
+		cachedAdjacentElements[element.id] = document.querySelector('#' + element.id + ' + label');
+
+		element.addEventListener('focus', function(e) {
+			if (tabbed) {
+				cachedAdjacentElements[e.target.id].classList.add('focused');
+				tabbed = false;
+			}
+		});
+
+		element.addEventListener('blur', function(e) {
+			cachedAdjacentElements[e.target.id].classList.remove('focused');
+		});
+	});
+});
+
+var selects = document.querySelectorAll('select');
+
+selects.forEach(function(select) {
+  select.addEventListener('focus', function(e) {
+    if (tabbed) {
+      e.target.parentElement.classList.add('focused');
+      tabbed = false;
+    }
+  });
+
+  select.addEventListener('blur', function(e) {
+    e.target.parentElement.classList.remove('focused');
+  });
+});
